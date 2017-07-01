4581b1df058b5f8e3e63f3f679901decb019c0a5
Luis Angel Perez Muñoz <ingluisperez.m@outlook.com>
2017-07-01 18:19:05 -0500

    Terminado

--- pokedex.js
+++ pokedex.js
@@ -1,10 +1,10 @@
 	$(document).ready(function(){
 		var tipos= {
-			"Pasto":"00B16A",
+			"Hierba":"00B16A",
 			"Veneno":"663399",
 			"Fuego":"EC644B",
 			"Volador":"E67E22",
-			"Acuatico":"22A7F0",
+			"Agua":"22A7F0",
 			"Bicho":"26A65B",
 			"Normal":"F7CA18",
 			"Electrico":"F4D03F",
@@ -17,196 +17,264 @@
 			"Dragon":"EF4836",
 			"Siniestro":"141414"
 		};
-
-		
 		var datos;
-			$('#exampleModal').on('show.bs.modal', function (event) {
-			  var tarjeta = $(event.relatedTarget) // Button that triggered the modal
-			  var index = tarjeta.data('whatever') // Extract info from data-* attributes
-			  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-			  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-			  var modal = $(this)
-			  modal.find('.nombre_modal').text(datos["pokemon"][index]["name"]);
-			  modal.find('.img_modal').attr("src",datos["pokemon"][index]["img"]);
-			  //modal.find('.img-modal').attr("src","img/"+datos.Laboratorios[index].Img);
-			  modal.find('.perfil_modal').empty();
-			  modal.find('.perfil_modal').append("<p>Peso: "+datos["pokemon"][index]["weight"]+"</p>");
-			  modal.find('.perfil_modal').append("<p>Altura: "+datos["pokemon"][index]["height"]+"</p>");
-			  modal.find('.perfil_modal').append("<p>Huevo a los : "+datos["pokemon"][index]["egg"]+"</p>");
-			  modal.find('.tipos_modal').empty();
-			  $.each(datos["pokemon"][index]["type"],function(index,value)
-	               			{	
+		//modal
+		$('#exampleModal').on('show.bs.modal', function (event) {
+		  var tarjeta = $(event.relatedTarget); 
+		  var index = tarjeta.data('whatever');  //index
+		  var modal = $(this);
 
-	               				var $tagTipo=$("<span></span>");
-	               				$tagTipo.append(value);
-	               				$tagTipo.css({"margin-left":"5px","background-color":"#"+tipos[value],"color":"white","padding":"3px","border-radius":"4px"});
-	               				modal.find('.tipos_modal').append($tagTipo);
+		  modal.find('.nombre_modal').text(datos["pokemon"][index]["name"]);
+		  modal.find('.img_modal').attr("src",datos["pokemon"][index]["img"]);
+		  
+		  modal.find('.perfil_modal').empty();
+		  modal.find('.perfil_modal').append("<p>Peso: "+datos["pokemon"][index]["weight"]+"</p>");
+		  modal.find('.perfil_modal').append("<p>Altura: "+datos["pokemon"][index]["height"]+"</p>");
+		  modal.find('.perfil_modal').append("<p>Huevo a los : "+datos["pokemon"][index]["egg"]+"</p>");
+		  
+		  modal.find('.tipos_modal').empty();
+		  $.each(datos["pokemon"][index]["type"],function(index,value)
+          {	
+   				var $tagTipo=$("<span></span>");
+   				$tagTipo.append(value);
+   				$tagTipo.css({"margin-left":"5px","background-color":"#"+tipos[value],"color":"white","padding":"3px","border-radius":"4px"});
+   				modal.find('.tipos_modal').append($tagTipo);
+          });
 
-	               			});
-			  modal.find('.debilidades_modal').empty();
-			  $.each(datos["pokemon"][index]["weaknesses"],function(index,value)
-	               			{	
-
-	               				var $tagTipo=$("<span></span>");
-	               				$tagTipo.append(value);
-	               				$tagTipo.css({"margin-left":"5px","background-color":"#"+tipos[value],"color":"white","padding":"3px","border-radius":"4px"});
-	               				modal.find('.debilidades_modal').append($tagTipo);
-
-	               			});
-			  modal.find('.evoluciones_anteriores_modal').empty();
-			   $.each(datos["pokemon"][index]["prev_evolution"],function(index,value)
-	               			{	
-
-	               				var $img=$("<img/>");
-	               				$img.attr({"src":datos["pokemon"][parseInt(value["num"]-1)]["img"]});
-	               				$img.css({"class":"img-responsive","width":"50px"});
-	               				modal.find('.evoluciones_anteriores_modal').append($img);
-
-	               			});
-			   modal.find('.evoluciones_siguientes_modal').empty();
-			    $.each(datos["pokemon"][index]["next_evolution"],function(index,value)
-	               			{	
-
-	               				var $img=$("<img/>");
-	               				$img.attr("src",datos["pokemon"][parseInt(value["num"]-1)]["img"]);
-	               				$img.css({"class":"img-responsive","width":"50px"});
-	               				modal.find('.evoluciones_siguientes_modal').append($img);
-
-	               	});
-			    if($("#"+index).hasClass("atrapado"))
-				{
-					$('#atrapalo').text("Dejar libre");
-				}else{
-					$('#atrapalo').text("Atrapar");
-				}
-
-			    $('#atrapalo').attr("name",index);
-
+		  modal.find('.debilidades_modal').empty();
+		  $.each(datos["pokemon"][index]["weaknesses"],function(index,value)
+			{	
+				var $tagTipo=$("<span></span>");
+				$tagTipo.append(value);
+				$tagTipo.css({"margin-left":"5px","background-color":"#"+tipos[value],"color":"white","padding":"3px","border-radius":"4px"});
+				modal.find('.debilidades_modal').append($tagTipo);
 			});
 
-		$("#atrapalo").click(function(e){
+		  modal.find('.evoluciones_anteriores_modal').empty();
+		  $.each(datos["pokemon"][index]["prev_evolution"],function(index,value)
+			{	
+				var $img=$("<img/>");
+				$img.attr({"src":datos["pokemon"][parseInt(value["num"]-1)]["img"]});
+				$img.css({"class":"img-responsive","width":"50px"});
+				modal.find('.evoluciones_anteriores_modal').append($img);
+			});
+
+		   modal.find('.evoluciones_siguientes_modal').empty();
+		    $.each(datos["pokemon"][index]["next_evolution"],function(index,value)
+   			{	
+   				var $img=$("<img/>");
+   				$img.attr("src",datos["pokemon"][parseInt(value["num"]-1)]["img"]);
+   				$img.css({"class":"img-responsive","width":"50px"});
+   				modal.find('.evoluciones_siguientes_modal').append($img);
+   			});
+
+   			//boton
+		    if($("#"+index).hasClass("atrapado"))
+			{
+				$('#atrapalo').text("Dejar libre");
+
+			}else{
+				$('#atrapalo').text("Atrapar");
+
+			}
+
+		    $('#atrapalo').attr("name",index);
+
+		});
+
+		//boton
+		$("#atrapalo").click(function(e){ 
 			e.preventDefault();
-			var index=$('#atrapalo').attr("name");
+			var index=$('#atrapalo').attr("name"); //obtengo el index del pokemon
 			$("#exampleModal").modal('hide');
-			$("#"+ $(this).attr("name")).toggleClass("atrapado");
-			if($("#"+ $(this).attr("name")).hasClass("atrapado"))
+			$("#"+ index).toggleClass("atrapado"); 
+
+			if($("#"+ index).hasClass("atrapado"))
 			{
 				crearCookie(index,escape(datos["pokemon"][index]["name"]));
 			}else{
 				eliminarCookie(index);
 			}
-
 		});
 
-		$('#filtrar').on("input",function(){
-                    var rex = new RegExp($(this).val(),'i');
-                    $objPokemones=datos.pokemon.filter(function(el){ 
-                        return getNombre(el,rex)
-                    });
-                	
-                	$("#pokemon-container").empty();
-                    crear_catalogo($objPokemones,tipos);
-                });
-	    
-			function getNombre(dato,rex){
-		        return rex.test(dato.name);
-            }
-		
+
+		$('#filtrar').on("input paste change propertychange",function()
+		{
+			var valores=$(this).val().split(":");
+			if(valores.length<2){
+		        var rex = new RegExp(valores[0],'i');
+		        $objPokemones=datos.pokemon.filter(function(el){ 
+		            return getNombre(el,rex)
+		        });
+		    	
+		    	$("#pokemon-container").empty();
+		        crear_catalogo($objPokemones,tipos);
+	    	}
+	    	else
+	    	{
+	    		var rex = new RegExp(valores[1],'i');
+		        $objPokemones=datos.pokemon.filter(function(el){ 
+		            return getValue(el,valores[0],rex)
+		        });
+		    	
+		    	$("#pokemon-container").empty();
+		        crear_catalogo($objPokemones,tipos);
+	    	}
+
+	    });
+
+		$("#todos").click(function(e){
+			e.preventDefault();
+			$("#filtrar").val("");
+			$("#filtrar").trigger("input");
+
+			
+		});
+
+		$("#atrapados").click(function(e){
+			e.preventDefault();
+			$("#pokemon-container").empty();
+			datosFiltrados=buscarAtrapados(datos);
+			console.log(datosFiltrados["pokemon"]);
+			crear_catalogo(datosFiltrados["pokemon"],tipos);
+
+			
+		});
+
+		function getNombre(dato,rex)
+		{
+	        return rex.test(dato.name);
+        }
+
+        function getValue(dato,propiedad,rex)
+        {
+        	if (propiedad=="tipo") {
+        		for(var i=0; i< dato["type"].length;i++)
+        		{
+        			if(rex.test(dato["type"][i]))
+        			{
+        				return true;
+        			}
+        		}
+
+        	}
+        	else
+        	{
+        		for(var i=0; i< dato["weaknesses"].length;i++)
+        		{
+        			if(rex.test(dato["weaknesses"][i]))
+        			{
+        				return true;
+        			}
+        		}
+        	}
+
+        	return false;
+        }
+
+		//obtener la info
 		$.getJSON("pokedex.json",function(value){
 			datos=value;
 			llenar_dropdowns(tipos);
 			crear_catalogo(datos["pokemon"],tipos);
-			
-			
-
 		});
 		
 		
 
 	});
 
-	function llenar_dropdowns(tipos)
+function llenar_dropdowns(tipos)
+{
+	
+	$.each(tipos,function(value,index)
 	{
 		
-			
-			$.each(tipos,function(index,value)
-			{
-				
-				$("#tipo").append("<li><a href='#'>"+index+"</a></li>");
-				$("#debilidad").append("<li><a href='#'>"+index+"</a></li>");
+		$("#tipo").append("<li><a onclick='buscar(\"tipo\",\""+value+"\")'>"+value+"</a></li>");
+		$("#debilidad").append("<li><a onclick='buscar(\"debilidad\",\""+value+"\")'>"+value+"</a></li>");
 
-			});
-	}
+	});
+}
 
-	function crear_catalogo(datos,tipos) 
+function buscar(opc,tipo)
+{
+	$("#filtrar").val(opc+":"+tipo);
+	$("#filtrar").trigger("input");
+}
+
+function crear_catalogo(datos,tipos) 
+{
+
+	var $contenedor = $("#pokemon-container");
+	var numeroFilas=datos.length/4;
+   	var pokemones=datos;
+   	var sobra=datos.length-(parseInt(numeroFilas)*4);
+
+   for(var i=0;i<parseInt(numeroFilas);i++)
+   {	
+   		var $nuevaFila=$("<div></div>");
+   		$nuevaFila.attr("class","row");
+   		for(var j=0;j<4;j++)
+   		{
+   			//creamos columnas
+   			var $nuevaCol=$("<div data-whatever='"+(parseInt(pokemones[(4*i)+j]["num"])-1)+"' data-toggle='modal' data-target='#exampleModal' id='"+(parseInt(pokemones[(4*i)+j]["num"])-1)+"'></div>");
+   			var $tipo=$("<div></div>");
+   			$nuevaCol.attr("class","col-md-3 col-sm-3 col-xs-6 text-center tarjeta");
+
+   			$nuevaCol.append("<div class='img cargador'><img data-src='"+ pokemones[(4*i)+j]["img"]+"' class='img-responsive post-carga center-block' /></div>");
+   			$nuevaCol.append("<h4>"+pokemones[(4*i)+j]["name"]+"</h4>");
+   			$.each(pokemones[(4*i)+j]["type"],function(index,value)
+   			{	
+   				var $tagTipo=$("<span></span>");
+   				$tagTipo.append(value);
+   				$tagTipo.css({"margin-left":"5px","background-color":"#"+tipos[value],"color":"white","padding":"3px","border-radius":"4px"});
+   				$tipo.append($tagTipo);
+   				$nuevaCol.append($tipo);
+
+   			});
+
+   			//agregamos a la fila
+   			$nuevaFila.append($nuevaCol);
+   		}
+   		//agregamos al contenedor
+   		$contenedor.append($nuevaFila);
+   }
+
+	var $nuevaFila=$("<div></div>");
+	$nuevaFila.attr("class","row");
+
+	for(var j=0;j<sobra;j++)
 	{
+		//creamos columnas
+		var $nuevaCol=$("<div data-whatever='"+(parseInt(pokemones[parseInt((numeroFilas))*4+j]["num"])-1)+"' data-toggle='modal' data-target='#exampleModal' id='"+(parseInt(pokemones[parseInt((numeroFilas))*4+j]["num"])-1)+"'></div>");
+		var $tipo=$("<div></div>");
+		$nuevaCol.attr("class","col-md-3 col-sm-3 col-xs-6 text-center tarjeta");
 
-		var $contenedor = $("#pokemon-container");
+		$nuevaCol.append("<div class='img cargador'><img data-src='"+ pokemones[(parseInt(numeroFilas)*4+j)]["img"]+"' class='img-responsive post-carga center-block' /></div>");
+		$nuevaCol.append("<h4>"+pokemones[(parseInt(numeroFilas)*4+j)]["name"]+"</h4>");
+		$.each(pokemones[(parseInt(numeroFilas)*4+j)]["type"],function(index,value)
+		{	
+			var $tagTipo=$("<span></span>");
+			$tagTipo.append(value);
+			$tagTipo.css({"margin-left":"5px","background-color":"#"+tipos[value],"color":"white","padding":"3px","border-radius":"4px"});
+			$tipo.append($tagTipo);
+			$nuevaCol.append($tipo);
 
-	               var numeroFilas=datos.length/4;
-	               var pokemones=datos;
-	               var sobra=datos.length-(parseInt(numeroFilas)*4);
-	               for(var i=0;i<parseInt(numeroFilas);i++)
-	               {	
-	               		var $nuevaFila=$("<div></div>");
-	               		$nuevaFila.attr("class","row");
-	               		for(var j=0;j<4;j++)
-	               		{
-	               			//creamos columnas
-	               			var $nuevaCol=$("<div data-whatever='"+(parseInt(pokemones[(4*i)+j]["num"])-1)+"' data-toggle='modal' data-target='#exampleModal' id='"+(parseInt(pokemones[(4*i)+j]["num"])-1)+"'></div>");
-	               			var $tipo=$("<div></div>");
-	               			$nuevaCol.attr("class","col-md-3 col-sm-3 col-xs-6 text-center tarjeta");
+		});
 
-	               			$nuevaCol.append("<div class='img cargador'><img data-src='"+ pokemones[(4*i)+j]["img"]+"' class='img-responsive post-carga center-block' /></div>");
-	               			$nuevaCol.append("<h4>"+pokemones[(4*i)+j]["name"]+"</h4>");
-	               			$.each(pokemones[(4*i)+j]["type"],function(index,value)
-	               			{	
-	               				var $tagTipo=$("<span></span>");
-	               				$tagTipo.append(value);
-	               				$tagTipo.css({"margin-left":"5px","background-color":"#"+tipos[value],"color":"white","padding":"3px","border-radius":"4px"});
-	               				$tipo.append($tagTipo);
-	               				$nuevaCol.append($tipo);
-
-	               			});
-
-	               			//agregamos a la fila
-	               			$nuevaFila.append($nuevaCol);
-	               		}
-	               		//agregamos al contenedor
-	               		$contenedor.append($nuevaFila);
-	               }
-	           		var $nuevaFila=$("<div></div>");
-	           		$nuevaFila.attr("class","row");
-	           		for(var j=0;j<sobra;j++)
-	           		{
-	           			//creamos columnas
-	           			var $nuevaCol=$("<div data-whatever='"+(parseInt(pokemones[parseInt((numeroFilas))*4+j]["num"])-1)+"' data-toggle='modal' data-target='#exampleModal' id='"+(parseInt(pokemones[parseInt((numeroFilas))*4+j]["num"])-1)+"'></div>");
-	           			var $tipo=$("<div></div>");
-	           			$nuevaCol.attr("class","col-md-3 col-sm-3 col-xs-6 text-center tarjeta");
-
-	           			$nuevaCol.append("<div class='img cargador'><img data-src='"+ pokemones[(parseInt(numeroFilas)*4+j)]["img"]+"' class='img-responsive post-carga center-block' /></div>");
-	           			$nuevaCol.append("<h4>"+pokemones[(parseInt(numeroFilas)*4+j)]["name"]+"</h4>");
-	           			$.each(pokemones[(parseInt(numeroFilas)*4+j)]["type"],function(index,value)
-	           			{	
-	           				var $tagTipo=$("<span></span>");
-	           				$tagTipo.append(value);
-	           				$tagTipo.css({"margin-left":"5px","background-color":"#"+tipos[value],"color":"white","padding":"3px","border-radius":"4px"});
-	           				$tipo.append($tagTipo);
-	           				$nuevaCol.append($tipo);
-
-	           			});
-
-	           			//agregamos a la fila
-	           			$nuevaFila.append($nuevaCol);
-	           		}
-	           		//agregamos al contenedor
-	           		$contenedor.append($nuevaFila);
-	           		cargarImagenes();
-					atraparPokemones();
+		//agregamos a la fila
+		$nuevaFila.append($nuevaCol);
 	}
+
+	//agregamos al contenedor
+	$contenedor.append($nuevaFila);
+	cargarImagenes();
+	atraparPokemones();
+}
 
 	// Crear Cookie
-var crearCookie = function (key, value) {
+var crearCookie = function (key, value) 
+{
+
     expires = new Date();
     expires.setTime(expires.getTime() + 31536000000); // Estableces el tiempo de expiración, genius
     cookie = key + "=" + value + ";expires=" + expires.toUTCString();
@@ -214,7 +282,9 @@ var crearCookie = function (key, value) {
 }
 
 // Leer Cookie
-var atraparPokemones = function () {
+var atraparPokemones = function () 
+{
+
     var pokemones = document.cookie.split(";");
     $.each(pokemones,function(index,value){
     	var valor = value.substring(0,value.indexOf("="));
@@ -222,8 +292,24 @@ var atraparPokemones = function () {
     });
 }
 
+function buscarAtrapados(datos)
+{
+	 var pokemones = document.cookie.split(";");
+	 var arreglo={
+	 	"pokemon":new Array()
+	 };
+    $.each(pokemones,function(index,value){
+    	var valor = value.substring(0,value.indexOf("="));
+
+    	arreglo.pokemon.push(datos.pokemon[valor.trim()]);
+    });
+  
+    return arreglo;
+}
+
 // Eliminar Cookie
-var eliminarCookie = function (key) {
+var eliminarCookie = function (key) 
+{
     return document.cookie = key + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 }
 	   
